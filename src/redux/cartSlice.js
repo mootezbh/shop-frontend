3efb2840b79819b6057e6fb40017b3e351ad804f
Mootez <benhammouda99@gmail.com>
2023-06-12 16:53:46 +0100

    update

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -3,7 +3,13 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    products: [],
+    products: [
+      {
+        product: null,
+        price: 0,
+        quantity: 0,
+      },
+    ],
     price_total: 0,
     quantity_total: 0,
   },
@@ -18,8 +24,8 @@ const cartSlice = createSlice({
         state.products[item_index].price =
           state.products[item_index].quantity * action.payload.price;
         for (let index = 0; index < state.products.length; index++) {
-            state.products
-            
+          state.products.price_total += state.products[index].price;
+          state.products.quantity_total += state.products[index].quantity;
         }
       }
     },
